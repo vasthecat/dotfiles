a673d9f79dfdbffb7a202d01227128b17a1e4006
Andrew Guschin <saintruler@gmail.com>
2020-11-30 20:38:18 +0400

    Changed application runner to rofi in xmonad

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -79,7 +79,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm,               xK_Return), spawn $ XMonad.terminal conf)
 
     -- launch dmenu
-    , ((modm,               xK_p     ), spawn "dmenu_run")
+    , ((modm,               xK_p     ), spawn "rofi -theme solarized_alternate -show drun")
 
     -- launch gmrun
     , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
