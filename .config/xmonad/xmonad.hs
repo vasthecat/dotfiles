23fe8dbf5535d6a4237eaf108b21cacd454f18b3
Andrew <saintruler@gmail.com>
2021-01-26 01:52:57 +0400

    Added gaps between windows in xmonad config

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -24,6 +24,7 @@ import XMonad.Hooks.EwmhDesktops
 
 -- Layout
 import XMonad.Layout.NoBorders
+import XMonad.Layout.Spacing
 
 -- Utils
 import XMonad.Util.Run -- (runProcessWithInput, safeSpawn, spawnPipe)
@@ -230,7 +231,7 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 -- The available layouts.  Note that each layout is separated by |||,
 -- which denotes layout choice.
 --
-myLayout = tiled ||| Mirror tiled ||| Full
+myLayout = spacing 10 $ tiled ||| Mirror tiled ||| Full
   where
      -- default tiling algorithm partitions the screen into two panes
      tiled   = Tall nmaster delta ratio
