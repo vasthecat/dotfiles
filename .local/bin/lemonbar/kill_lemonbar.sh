b815b8dc30a9632e159bf4347d615b067503215f
Andrew Guschin <saintruler@gmail.com>
2021-11-15 15:31:17 +0400

    Removed unneeded scripts

--- .local/bin/lemonbar/kill_lemonbar.sh
+++
@@ -1,6 +0,0 @@
-pids=$(pgrep -f "sh .*run_lemonbar")
-for pid in $pids ; do
-    kill $pid
-done
-pid=$(pgrep "lemonbar")
-kill $pid
--- .local/bin/lemonbar/modules/clock.py
+++
@@ -1,6 +0,0 @@
-from time import strftime
-
-
-def callback():
-    clock = strftime("%d %b (%a) %H:%M")
-    return f"CLK: {clock}"
